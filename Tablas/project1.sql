2e8d14a6d10c7fa50494a8b09ee7d3b6316774dc
Camilo Nossa <79663354+alejandronoss1017@users.noreply.github.com>
2022-02-28 15:39:06 -0500

    Correcion de sintaxis y agregacion de condiciones CHECK y DEFAULT

--- Tablas/project1.sql
+++ Tablas/project1.sql
@@ -7,7 +7,7 @@ CREATE TABLE clientes(
     fechaNacimiento DATE NOT NULL,
     fechaPrimeraVinculacion DATE,
     email VARCHAR2(60) NOT NULL,  --TODO: REGEXP_LIKE Para Emails validos
-    genero CHAR(1) NOT NULL, --TODO: Unicas Opciones F y M
+    genero CHAR(1) CHECK('F' OR 'M') NOT NULL,
     PRIMARY KEY(codigoCLiente)
 );
 
@@ -15,44 +15,44 @@ CREATE TABLE oficinas(
     codigoOficina NUMBER(30) NOT NULL,
     nombre VARCHAR2(60) NOT NULL,
     presupuesto NUMBER(20,2) NOT NULL,
-    horarioAdicional CHAR(1) NOT NULL, --TODO: Valores permitidos S(si) y N(no)
-    PRIMARY KEY(codigoOficina),
+    horarioAdicional CHAR(1) CHECK ('S' OR 'N') NOT NULL,
+    PRIMARY KEY(codigoOficina)
 );
 
 CREATE TABLE cuentas(
     numeroCuenta NUMBER(3,0) NOT NULL,
-    tipo CHAR(1) NOT NULL, --TODO: Valores permitidos A(Ahorros) o C(Corriente)
+    tipo CHAR(1) CHECK('A' OR 'C') NOT NULL,
     codigoOficina NUMBER(3,0) NOT NULL,
-    saldo NUMBER(20,0), --TODO: Saldo debe crearse con NULL
+    saldo NUMBER(20,0) DEFAULT NULL,
     PRIMARY KEY(numeroCuenta),
-    FOREING KEY(codigoOficina) REFERENCES oficinas
+    FOREIGN KEY (codigoOficina) REFERENCES oficinas
 );
 
 CREATE TABLE titulares(
     codigoCLiente NUMBER(3,0) NOT NULL,
     numeroCuenta NUMBER(3,0) NOT NULL,
     porcentajeTitularidad NUMBER(3,0) NOT NULL,
-    RIMARY KEY(codigoCLiente,numeroCuenta),
-    FOREING KEY(codigoCLiente) REFERENCES clientes,
-    FOREING KEY(numeroCuenta) REFERENCES cuentas
+    PRIMARY KEY(codigoCLiente,numeroCuenta),
+    FOREIGN KEY(codigoCLiente) REFERENCES clientes,
+    FOREIGN KEY(numeroCuenta) REFERENCES cuentas
 );
 
 CREATE TABLE pqrs(
     codigoCLiente NUMBER(3,0) NOT NULL,
     numero NUMBER(2,0) NOT NULL, 
-    tipoQueja CHAR(1) NOT NULL, --TODO: Valores permitidos son P,Q,R,S
+    tipoQueja CHAR(1) CHECK('P' OR 'Q' OR 'R' OR 'S') NOT NULL,
     descripcion VARCHAR2(2000) NOT NULL,
     PRIMARY KEY(codigoCLiente, numero),
-    FOREING KEY(codigoCLiente) REFERENCES clientes
+    FOREIGN KEY(codigoCLiente) REFERENCES clientes
 );
 
 CREATE TABLE movimientos(
     numeroCuenta NUMBER(3,0) NOT NULL,
     numero NUMBER(3,0) NOT NULL,
-    tipo CHAR(1) NOT NULL, --TODO: Posibles valores D(debitos),C(creditos),I(impuestos),R(rendimientos)
-    naturaleza CHAR(1) NOT NULL, --TODO: Valores permitidos A (automatico) y U (usuario)
+    tipo CHAR(1) CHECK('D' OR 'C' OR 'I' OR 'R') NOT NULL,
+    naturaleza CHAR(1) CHECK('A' OR 'U') NOT NULL,
     valor NUMBER(10,2) NOT NULL,
-    fechaMovimiento DATE NOT NULL,
+    fechaMovimiento DATETIME NOT NULL,
     PRIMARY KEY(numeroCuenta,numero),
-    FOREING KEY(numeroCuenta) REFERENCES cuentas
+    FOREIGN KEY(numeroCuenta) REFERENCES cuentas
 );
